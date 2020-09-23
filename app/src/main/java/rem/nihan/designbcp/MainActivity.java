f93d788fb09b8c1f05a2958b79fd7e8a4ddd5f5a
Gohan19266 <johanmamani@upeu.edu.pe>
2020-09-23 17:45:58 -0500

    Initial commit

---
+++ app/src/main/java/rem/nihan/designbcp/MainActivity.java
@@ -0,0 +1,14 @@
+package rem.nihan.designbcp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
